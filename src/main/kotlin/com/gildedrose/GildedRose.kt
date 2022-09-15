2392eeadd653c597122bf4c3df4965d16a8d6392
Darío Scattolini <dario.scattolini@gmail.com>
2022-09-16 00:36:48 +0200

    refactor production code: extract updateBackstagePasses method

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -20,23 +20,27 @@ class GildedRose(var items: Array<Item>) {
             var qualityVariationAmount = if (item.sellIn < 0) 2 else 1
 
             if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
-                qualityVariationAmount = when (item.sellIn) {
-                    in 11..POSITIVE_INFINITY.toInt() -> 1
-                    in 6..10 -> 2
-                    in 0..5 -> 3
-                    else -> item.quality
-                }
-
-                if (item.sellIn >= 0) {
-                    increaseQualityBy(item, qualityVariationAmount)
-                } else {
-                    decreaseQualityBy(item, qualityVariationAmount)
-                }
+                updateBackstagePasses(item)
             } else if (item.name == "Aged Brie") {
                 increaseQualityBy(item, qualityVariationAmount)
             } else {
                 decreaseQualityBy(item, qualityVariationAmount)
             }
+        }
+    }
+
+    private fun updateBackstagePasses(item: Item) {
+        val qualityVariationAmount = when (item.sellIn) {
+            in 11..POSITIVE_INFINITY.toInt() -> 1
+            in 6..10 -> 2
+            in 0..5 -> 3
+            else -> item.quality
+        }
+
+        if (item.sellIn >= 0) {
+            increaseQualityBy(item, qualityVariationAmount)
+        } else {
+            decreaseQualityBy(item, qualityVariationAmount)
         }
     }
 
