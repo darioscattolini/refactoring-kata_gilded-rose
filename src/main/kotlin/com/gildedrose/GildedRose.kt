6fa9ef8b00f115218333bf34e36e62b64d96c610
Darío Scattolini <dario.scattolini@gmail.com>
2022-09-16 00:46:35 +0200

    refactor production code: conflate quality modification functions into modifyQualityBy

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -3,13 +3,6 @@ package com.gildedrose
 import kotlin.Double.Companion.POSITIVE_INFINITY
 
 class GildedRose(var items: Array<Item>) {
-    private fun decreaseQualityBy(item: Item, amount: Int) {
-        item.quality = if (item.quality - amount >= 0) item.quality - amount else 0
-    }
-
-    private fun increaseQualityBy(item: Item, amount: Int) {
-        item.quality = if (item.quality + amount <= 50) item.quality + amount else 50
-    }
 
     fun updateQuality() {
         for (item in items) {
@@ -17,13 +10,21 @@ class GildedRose(var items: Array<Item>) {
 
             item.sellIn--
 
-            val qualityVariationAmount = if (item.sellIn < 0) 2 else 1
+            val qualityVariationAmount = if (item.sellIn < 0) -2 else -1
 
             when (item.name) {
                 "Backstage passes to a TAFKAL80ETC concert" -> updateBackstagePasses(item)
-                "Aged Brie" -> increaseQualityBy(item, qualityVariationAmount)
-                else -> decreaseQualityBy(item, qualityVariationAmount)
+                "Aged Brie" -> modifyQualityBy(item, -qualityVariationAmount)
+                else -> modifyQualityBy(item, qualityVariationAmount)
             }
+        }
+    }
+
+    private fun modifyQualityBy(item: Item, amount: Int) {
+        item.quality = when (item.quality + amount) {
+            in 50..POSITIVE_INFINITY.toInt() -> 50
+            in 0..49 -> item.quality + amount
+            else -> 0
         }
     }
 
@@ -32,14 +33,10 @@ class GildedRose(var items: Array<Item>) {
             in 11..POSITIVE_INFINITY.toInt() -> 1
             in 6..10 -> 2
             in 0..5 -> 3
-            else -> item.quality
+            else -> -item.quality
         }
 
-        if (item.sellIn >= 0) {
-            increaseQualityBy(item, qualityVariationAmount)
-        } else {
-            decreaseQualityBy(item, qualityVariationAmount)
-        }
+        modifyQualityBy(item, qualityVariationAmount)
     }
 }
 
